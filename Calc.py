65159dbcda048c1166fea821c0997900b716ec14
Normo! <navindusenehas@proton.me>
2024-12-20 17:44:26 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,6 +1,8 @@
+# In case if you did not saw the readme, read the note down below
+
 # NOTE: You have to make these changes to the line 10,11,12 to actually make it work
 
-# Add the path of the Moon-icon, Sun-icon, History-icon by removing the fake path and adding your paths of those files (You can download them via a zip file in this repo)
+# Add the path of the Moon-icon, Sun-icon, History-icon by removing the fake path and adding your paths of those files (You can download them via a zip file in this repo or make your own ones)
 
 import tkinter as tk
 from tkinter import PhotoImage
